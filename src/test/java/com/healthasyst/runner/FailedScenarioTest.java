666556e8b61e1d6133c34e1491eb61c3c6d3b626
balaji-githubstore <balaji0017@gmail.com>
2022-02-07 11:14:54 +0530

    to run failed scenario -rerun.txt

--- src/test/java/com/healthasyst/runner/FailedScenarioTest.java
+++ src/test/java/com/healthasyst/runner/FailedScenarioTest.java
@@ -5,23 +5,15 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-
 @CucumberOptions(
-		features = {"src/test/resources/feature"}
+		features = {"@target/rerun.txt"}
 		,glue = {"com.healthasyst.stepdefn","com.healthasyst.base"}
 		,publish = true
 		,monochrome = true
-//		,dryRun = true 
-//		,tags = "@login and (not @high)"
-//		,tags="@patient"
-		,tags="@login"
-//		,plugin = {"pretty","progress"} 
-//		,plugin = {"pretty","html:target/index.html","json:target/cucumber-report.json"} 
-		
 		,plugin = {"pretty","html:target/index.html","rerun:target/rerun.txt"} 
 		)
 
 @RunWith(Cucumber.class)
-public class RunnerTest {
+public class FailedScenarioTest {
 
 }
