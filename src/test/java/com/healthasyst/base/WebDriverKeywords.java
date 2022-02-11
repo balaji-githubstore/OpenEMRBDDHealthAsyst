c09b82bb9c46fe1b147331983f7857f36af0f9de
balaji-githubstore <balaji0017@gmail.com>
2022-02-11 08:38:48 +0530

    keywords sample - handle elements when multiple page loaders on a single page.

--- src/test/java/com/healthasyst/base/WebDriverKeywords.java
+++ src/test/java/com/healthasyst/base/WebDriverKeywords.java
@@ -2,6 +2,9 @@ package com.healthasyst.base;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.time.Duration;
+import java.util.NoSuchElementException;
+import java.util.function.Function;
 
 import org.apache.poi.ss.usermodel.DataFormatter;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -10,6 +13,10 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 
 public class WebDriverKeywords {
 	
@@ -28,6 +35,108 @@ public class WebDriverKeywords {
 	public void typeOnElement(By locator,String text)
 	{
 		driver.findElement(locator).sendKeys(text);
+	}
+	
+	public void clickUntilNextLocator(By loc1,By loc2,int count) throws Exception {
+		
+		int check=1;
+		Exception exc=null;
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+		while(driver.findElements(loc2).size()>0 && check<=80)
+		{
+			try
+			{
+				if(check==count)
+				{
+					break;
+				}
+				check++;
+				clickElement(loc1);
+				exc=null;
+				
+			}
+			catch (Exception e) {
+				exc=e;
+			}
+		}
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		if(count==30 && exc!=null)
+		{
+			throw exc;
+		}
+	}
+	
+	public void clickMultipleTimes(By loc,int count) throws Exception {
+		
+		int check=1;
+		Exception exc=null;
+		int pollingInterval = 500;
+		int timeOut = 60;
+		boolean verify=false;
+		while(check<=60)
+		{
+			try
+			{
+				if(check==count)
+				{
+					break;
+				}
+				
+				Wait<WebDriver> wait = new FluentWait<WebDriver>(driver)
+						.withTimeout(Duration.ofSeconds(timeOut)).pollingEvery(Duration.ofSeconds(pollingInterval))
+						.ignoring(Exception.class);
+				
+				WebElement clickseleniumlink = wait.until(new Function<WebDriver, WebElement>(){
+					
+					public WebElement apply(WebDriver driver ) {
+						return driver.findElement(loc);
+					}
+				});
+				
+				clickseleniumlink.click();
+					
+				//click
+				exc=null;
+				break;
+			}
+			catch(Exception ex)
+			{
+				exc=ex;
+			}
+		}
+		
+		if(count==30 && exc!=null && verify!=true)
+		{
+			throw exc;
+		}
+	}
+	
+public void clickUntilNextLocatorDisplayed(By loc1,By loc2,int count) throws Exception {
+		
+		int check=1;
+		Exception exc=null;
+		while(driver.findElement(loc2).isDisplayed() && check<=30)
+		{
+			try
+			{
+				if(check==count)
+				{
+					break;
+				}
+				check++;
+				clickElement(loc1);
+				exc=null;
+//				break;
+			}
+			catch (Exception e) {
+				exc=e;
+			}
+		}
+		
+		if(count==30)
+		{
+			throw exc;
+		}
 	}
 
 	public static Object[][] sheetToObject(String path,String sheetName) throws IOException {
